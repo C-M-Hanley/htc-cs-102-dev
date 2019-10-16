6ce379660c8a61505f6047a04db74085b3c69b15
User1 <User1@RDV-005604>
2019-10-16 19:25:54 -0400

    Improved MovieList project

--- Week03/MovieList/MovieList/Movie.cs
+++ Week03/MovieList/MovieList/Movie.cs
@@ -11,6 +11,9 @@ namespace MovieList
     {
         public string Title;
         public int ReleaseYear;
+        public string Director;
+        public int Length;
+        public string Genre;
 
         public Movie(string title, int releaseYear)
         {
@@ -18,9 +21,33 @@ namespace MovieList
             ReleaseYear = releaseYear;
         }
 
+        public Movie(string title, int releaseYear, string director)
+        {
+            Title = title;
+            ReleaseYear = releaseYear;
+            Director = director;
+        }
+
+        public Movie(string title, int releaseYear, string director, int length)
+        {
+            Title = title;
+            ReleaseYear = releaseYear;
+            Director = director;
+            Length = length;
+        }
+
+        public Movie(string title, int releaseYear, string director, int length, string genre)
+        {
+            Title = title;
+            ReleaseYear = releaseYear;
+            Director = director;
+            Length = length;
+            Genre = genre;
+        }
+
         public void ShowDetails()
         {
-            MessageBox.Show(Title + " was released in " + ReleaseYear)
+            MessageBox.Show(Title + " was released in " + ReleaseYear + "\n\r" + "Director is " + Director + "\n\r" + "Length is " + Length + "\n\r" + "Genre is " + Genre);
         }
     }
 }
